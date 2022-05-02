767ab46ac319fb56ee3edc0b5a74767878bbb4f6
MaryOp <mar-opolchentseva@yandex.ru>
2022-05-02 08:57:49 +0300

    feat: first 3.5 from 21

--- src/dream-team.js
+++ src/dream-team.js
@@ -13,9 +13,50 @@ const { NotImplementedError } = require('../extensions/index.js');
  * createDreamTeam(['Olivia', 1111, 'Lily', 'Oscar', true, null]) => 'LOO'
  *
  */
-function createDreamTeam(/* members */) {
-  throw new NotImplementedError('Not implemented');
+function createDreamTeam(members) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+    let members_arr = [];  
+    if (((typeof members) !== 'object') | (members == null)) {
+      return false
+    }
+    members_arr = Array.from(members);
+    let DreamTeam = '';
+    let result_arr = [];
+    if ((typeof members_arr !== 'object') | (Array.isArray(members_arr) == false)) {
+      return false;
+    }
+
+      function isUndefined(input) {
+        for (let i = 0; i < input.length; i++) {
+          if ((typeof input == undefined) | (input == undefined) | (input == '')) {
+          return true;
+          }}}
+      if (!((members_arr == []) && (members_arr == '') && ((members_arr.some(isUndefined) == true) | (members_arr.some(isUndefined) == undefined)))) {
+ 
+        if (members_arr == '') {
+          return false
+        }
+           members_arr.forEach((element) => {
+                if (typeof element == 'string') {        
+                  element = element.trimStart();  
+                      let el_arr = Array.from(element.toUpperCase())[0];
+                      result_arr.push(el_arr);
+                }
+          })
+    }
+    else {
+      console.log('55')
+      return false;
+    }
+
+    if (result_arr.some(isUndefined)) {
+      return false;
+    }
+
+    DreamTeam = result_arr.sort();
+    DreamTeam = DreamTeam.join('');
+    return DreamTeam;
 }
 
 module.exports = {
--- src/what-season.js
+++ src/what-season.js
@@ -11,11 +11,44 @@ const { NotImplementedError } = require('../extensions/index.js');
  * getSeason(new Date(2020, 02, 31)) => 'spring'
  * 
  */
-function getSeason(/* date */) {
-  throw new NotImplementedError('Not implemented');
+function getSeason(date) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  if (date == '' | date == undefined) {
+    return 'Unable to determine the time of year!'
+  };
+  // console.log(typeof date + ' ' + date)
+  
+if ((typeof date !== 'object') | (typeof date.valueOf() !== 'number') | (date == Date.now())){
+  return 'Invalid date!';
+}
+
+  let month = date.getMonth();
+  switch (month) {
+    case 0:
+    case 1: 
+    case 11:
+      return 'winter';
+    case 3:
+    case 4:
+    case 2:
+      return 'spring';
+    case 6:
+    case 7:
+    case 5:
+      return 'summer';
+    case 9:
+    case 10:
+    case 8:
+      return 'autumn';
+    default:
+      throw new NotImplementedError ("Invalid date!");
+  }
+
 }
 
 module.exports = {
   getSeason
 };
+
+// console.log(getSeason(new Date()))
