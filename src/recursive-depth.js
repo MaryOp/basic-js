9dd744c0f67c19e70a295b4a57ae843a3705f444
MaryOp <mar-opolchentseva@yandex.ru>
2022-05-02 12:36:22 +0300

    feat: next try +2

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -13,12 +13,36 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  */
 class DepthCalculator {
-  calculateDepth(/* arr */) {
-    throw new NotImplementedError('Not implemented');
+  calculateDepth(arr) {
+    // throw new NotImplementedError('Not implemented');
     // remove line with error and write your code here
+    let depth = 1;
+    for (let i = 0; i < arr.length; i++) {
+      if (Array.isArray(arr[i]) == true) {
+        console.log(i + " " + arr[i] + " " + this.calculateDepth(arr[i]))
+        // if (this.calculateDepth(arr[i]) !== 1) {
+        depth+=this.calculateDepth(arr[i]);
+        // }
+        // else {
+          // depth +=0;
+        // }
+      }
+      else {
+        if (Array.isArray(arr[i]) == false) {
+          depth+=0;
+          // return depth;
+        }
+      }
+    }
+
+      return depth;
   }
+ 
 }
 
 module.exports = {
   DepthCalculator
 };
+
+const depthCalc = new DepthCalculator();
+console.log(depthCalc.calculateDepth(([1, 2, 3, [1], 4, 5, [1]])))
--- src/transform-array.js
+++ src/transform-array.js
@@ -13,11 +13,51 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  // throw new NotImplementedError('Not implemented');
   // remove line with error and write your code here
+  let resultArr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+  // arr.forEach ((element) => {
+    console.log(typeof arr[i]);
+    console.log((typeof arr[i]) == 'number');
+    switch (arr[i]) {
+      case ((typeof arr[i]) == 'number'):
+        console.log('tyt');
+        resultArr = resultArr.push(arr[i]);
+        break;
+      case ((typeof arr[i]) == 'string'):
+        switch (arr[i]) {
+          case '--discard-prev':
+          resultArr = resultArr.pop();
+          break;
+
+          case '--discard-next':
+            i+= 1;
+            break;
+
+          case '--double-prev':
+          resultArr = resultArr.push(resultArr[resultArr.length - 1]);
+          break;
+
+          case '--double-next':
+
+          default:
+            throw new Error("\'arr\' parameter must be an instance of the Array!");
+    }
+    break;
+    default:
+            throw new Error("\'arr\' parameter must be an instance of the Array!");
+
+    }
+  // })
+}
+  return resultArr;
 }
 
-module.exports = {
-  transform
-};
+// module.exports = {
+//   transform
+// };
+
+// console.log(transform([1, 2, 3, '--double-prev', 4, 5]))
